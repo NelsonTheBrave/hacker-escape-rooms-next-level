f7b28356f98630e194c1992a100c0d86625db4c3
JrSchoolAccount <jesperx2000@gmail.com>
2023-11-17 09:16:57 +0100

    Function to fetch & render challenges WiP

--- challenges.js
+++ challenges.js
@@ -1,7 +1,22 @@
-// Fetch resources from API
+// Fetch resources from API and render challenges in cards
 async function createChallenges() {
   const res = await fetch(
     "https://lernia-sjj-assignments.vercel.app/api/challenges"
   );
   const data = await res.json();
+
+  const challenges = data.challenges;
+
+  let challengesRender = challenges;
+
+  const challengesCard = document.querySelector(
+    ".challenges-container__challenge"
+  );
+
+  challengesRender.forEach((challenge) => {
+    //  Create elements to put data into
+    //  add classes to elements
+    //  do this for every challenge in challengeRender.
+    //
+  });
 }
