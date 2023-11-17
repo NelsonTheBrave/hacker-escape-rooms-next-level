8fc5ecfe724b57fbef2c404e0bb8f5d0d6ae7c2b
JrSchoolAccount <jesperx2000@gmail.com>
2023-11-17 16:17:39 +0100

    Almost done with the js for the cards

--- challenges.js
+++ challenges.js
@@ -1,22 +1,60 @@
-// Fetch resources from API and render challenges in cards
-async function createChallenges() {
+async function getApi() {
   const res = await fetch(
     "https://lernia-sjj-assignments.vercel.app/api/challenges"
   );
   const data = await res.json();
+  challenges = data.challenges;
+  renderChallenges(challenges);
+}
 
-  const challenges = data.challenges;
-
+getApi();
+// Render challenges in cards
+function renderChallenges(challenges) {
   let challengesRender = challenges;
+  const challengesContainer = document.querySelector(".challenges-container");
 
-  const challengesCard = document.querySelector(
-    ".challenges-container__challenge"
-  );
+  challengesRender.forEach((challenges) => {
+    // Create challenge card
+    const challengeCard = document.createElement("div");
+    const titleDiv = document.createElement("div");
+    const title = document.createElement("h3");
+    const img = document.createElement("img");
+    const rating = document.createElement("small");
+    const participants = document.createElement("span");
+    const challengeText = document.createElement("p");
+    const challengeButton = document.createElement("button");
 
-  challengesRender.forEach((challenge) => {
-    //  Create elements to put data into
-    //  add classes to elements
-    //  do this for every challenge in challengeRender.
-    //
+    // Add classes to challenge card elements
+    challengeCard.classList.add("challenges-container__challenge");
+    titleDiv.classList.add("challenges-container__challenge__lowerWrapper");
+    title.classList.add(".challenges-container__challenge__title");
+    img.classList.add("challenges-container__challenge__img");
+    rating.classList.add(".challenges-container__challenge__rating");
+    participants.classList.add(
+      "challenges-container__challenge__rating__participants"
+    );
+    challengeText.classList.add("challenges-container__challenge__text");
+    challengeButton.classList.add("challenges-container__challenge__button");
+
+    // Add content to challenge card elements
+    challengeCard.id = challenges.id;
+    title.textContent = challenges.title;
+    img.src = challenges.image;
+    participants.textContent =
+      challenges.minParticipants === challenges.maxParticipants
+        ? `${challenges.minParticipants} participants`
+        : `${challenges.minParticipants}-${challenges.maxParticipants} participants`;
+    challengeText.textContent = challenges.description;
+    challengeButton.textContent = "Book this room";
+
+    // Append challenge elements
+    challengesContainer.appendChild(challengeCard);
+    challengeCard.appendChild(img);
+    challengeCard.appendChild(titleDiv);
+    titleDiv.appendChild(title);
+    challengeCard.appendChild(rating);
+    rating.appendChild(participants);
+    challengeCard.appendChild(challengeText);
+    challengeCard.appendChild(challengeButton);
   });
 }
