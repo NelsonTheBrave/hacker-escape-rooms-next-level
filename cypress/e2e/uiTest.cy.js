5791266951454df78d074c68b0a79b46f0945724
JrSchoolAccount <jesperx2000@gmail.com>
2024-03-05 10:51:23 +0100

    Added more intricate UI test

--- cypress/e2e/uiTest.cy.js
+++ cypress/e2e/uiTest.cy.js
@@ -1,6 +1,42 @@
-describe('Visit the site on localhost:5500', () => {
-  it('Site should be runing on localhost:5500', () => {
-    cy.visit('http://localhost:5500');
+describe('Visit the site on GitHub pages', () => {
+  it('Site should be running on GitHub pages', () => {
+    cy.visit(
+      'https://nelsonthebrave.github.io/hacker-escape-rooms-next-level/'
+    );
     cy.get('h1').should('exist');
+  });
+});
+
+describe('Visit landing page and navigate to online challenges and change filter', () => {
+  it('Should navigate across the site with ease', () => {
+    cy.visit(
+      'https://nelsonthebrave.github.io/hacker-escape-rooms-next-level/'
+    );
+    cy.wait(900);
+
+    cy.get('.buttons__onlineBtn').first().click();
+    cy.url().should(
+      'eq',
+      'https://nelsonthebrave.github.io/hacker-escape-rooms-next-level/challenges.html?type=online'
+    );
+
+    cy.wait(900);
+    cy.get('.filterButton').click();
+
+    cy.get('#linux').click();
+
+    cy.get('#13')
+      .should('have.class', 'linux')
+      .and('not.have.css', 'display', 'none');
+
+    cy.get('#electronics').click();
+
+    cy.get('.no-match-message').should('not.have.css', 'display', 'none');
+
+    cy.get('.logo').click();
+    cy.url().should(
+      'eq',
+      'https://nelsonthebrave.github.io/hacker-escape-rooms-next-level/index.html'
+    );
   });
 });
